bf8f196d0dc822b596fe0e6a4265fab358c7b187
Md Sayed Uddin <sayedrayhan10@gmail.com>
2025-11-21 22:09:01 +0600

    Clear parameter vs argument

--- functions/user_define_function.py
+++ functions/user_define_function.py
@@ -1,27 +1,27 @@
 
 # Input + return
-def add_two_numbers(a, b): # Function definition with arguments a and b
+def add_two_numbers(a, b): # Function definition with parameter a and b
     return a + b
 
-result = add_two_numbers(5, 7) # Function call with parameters 5 and 7
+result = add_two_numbers(5, 7) # Function call with arguments 5 and 7
 print("The sum is:", result)
 
 
 # No input + return
-def get_greeting(): # Function definition with no arguments
+def get_greeting(): # Function definition with no parameters
     return "Hello, welcome to Python functions!"
 
 greeting = get_greeting() # Function call
 print(greeting)
 
 # Input + no return
-def print_square(num): # Function definition with argument num
+def print_square(num): # Function definition with parameter num
     print("The square of", num, "is", num ** 2)
 
-print_square(4) # Function call with parameter 4
+print_square(4) # Function call with argument 4
 
 # No input + no return
-def display_message(): # Function definition with no arguments
+def display_message(): # Function definition with no parameters
     print("This is a simple message from a function.")
 
 display_message() # Function call
